43d830b17808f719ae713c7cb6989e687cd1de7d
tprdk <61751375+tprdk@users.noreply.github.com>
2020-07-22 23:15:47 +0300

    Update Solution.cs

--- two-sum/Solution.cs
+++ two-sum/Solution.cs
@@ -1,1 +1,22 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        
+        int[] array = {0, 0};
+        int i = 0;
+        Boolean flag = true;
+        Dictionary<int, int> complementMap = new Dictionary<int, int>();
 
+        while (flag){
+            if(complementMap.ContainsKey(nums[i])){
+                array[0] = complementMap[nums[i]];
+                array[1] = i;
+                flag = false;
+            }else{
+                if(!complementMap.ContainsKey(target - nums[i]))
+                    complementMap.Add(target - nums[i], i);
+                i++;
+            }
+        }
+        return array;
+    }
+}
